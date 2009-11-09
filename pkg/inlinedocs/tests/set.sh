604cb815ded1e6cddb9999eb41f5fa047b92bdd0
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-09 18:22:20 +0000

    description of set.sh
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inlinedocs/tests/set.sh
+++ pkg/inlinedocs/tests/set.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Set unit tests output file to current output.
+# This should only be run when we add unit tests or
+# intentionally change the output of the package.
 R CMD INSTALL ..
 R --no-save 2>&1 <unit.tests.R|tee unit.tests.Rout.save 
