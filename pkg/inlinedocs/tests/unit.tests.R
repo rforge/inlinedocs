5ed7d79fab8694c39981c91f07d9bf0ccb18e13c
kmpont <kmpont@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-26 16:45:50 +0000

    Fix unit tests to use same file order on all locales
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@59 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inlinedocs/tests/unit.tests.R
+++ pkg/inlinedocs/tests/unit.tests.R
@@ -3,6 +3,11 @@ library(inlinedocs)
 test.pkg <- function(pkg){
   package.skeleton.dx(pkg)
   Rd.files <- Sys.glob(file.path(pkg,"man","*"))
+  ## now sort these in locale-independent manner
+  loc <- Sys.getlocale(category = "LC_COLLATE")
+  Sys.setlocale(category="LC_COLLATE","C")
+  Rd.files <- sort(Rd.files)
+  Sys.setlocale(category="LC_COLLATE",loc)
   lapply(Rd.files,function(f)cat(paste(readLines(f),collapse="\n")))
 }
 pkgs <- rownames(subset(file.info(dir()),isdir==TRUE))
