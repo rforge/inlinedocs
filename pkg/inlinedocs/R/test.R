ce242cc1fb9634b84f3ba850fd0956079c8096c3
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-18 09:40:30 +0000

    test.file verbose, tags test, save.test.result doc
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@113 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inlinedocs/R/test.R
+++ pkg/inlinedocs/R/test.R
@@ -6,21 +6,27 @@ test.file <- function
 ### extract.docs.file to the file. We check for identity of elements
 ### of elements of the list, so the order of elements should not
 ### matter, and thus this should be a good robust unit test.
-(f
+(f,
 ### File name of R code file with inlinedocs to parse and check.
+ verbose=TRUE
+### Show output?
  ){
   result <- extract.docs.file(f)
   e <- new.env()
   sys.source(f,e)
   ## these are the items to check for, in no particular order
   .result <- e$.result
-  for(FUN in names(.result))for(N in names(.result[[FUN]])){
-    .res <- .result[[FUN]][[N]]
-    res <- result[[FUN]][[N]]
-    if(is.null(res) || .res!=res){
-      stop(f,":\n\n",res,"\nin ",FUN,"$",N,", expected:\n\n",.res,"\n")
+  for(FUN in names(.result)){
+    if(verbose)cat(FUN,"")
+    for(N in names(.result[[FUN]])){
+      .res <- .result[[FUN]][[N]]
+      res <- result[[FUN]][[N]]
+      if(is.null(res) || .res!=res){
+        stop(f,":\n\n",res,"\nin ",FUN,"$",N,", expected:\n\n",.res,"\n")
+      }
     }
   }
+  if(verbose)cat("\n")
 }
 save.test.result <- function
 ### For unit tests, this is an easy way of getting a text
@@ -28,8 +34,8 @@ save.test.result <- function
 (f
 ### R code file with inlinedocs to process with extract.docs.file.
  ){
-  L <- extract.docs.file(f)
-  dump("L",control=NULL)
+  .result <- extract.docs.file(f)
+  dump(".result",control=NULL)
   lines <- readLines("dumpdata.R")
-  cat(lines)
+  cat(paste(lines,"\n"))
 }
--- pkg/inlinedocs/tests/unit.tests.R
+++ pkg/inlinedocs/tests/unit.tests.R
@@ -1,4 +1,4 @@
 tdir <- system.file("testfiles",package="inlinedocs")
 testfiles <- Sys.glob(file.path(tdir,"*.R"))
 library(inlinedocs)
-for(f in testfiles)test.file(f)
+for(f in testfiles)test.file(f,verbose=FALSE)
