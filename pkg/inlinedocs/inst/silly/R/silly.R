167dce368f8e85c4b2d0495cd44ac68a78fb5439
tomaschwutz <tomaschwutz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-08-30 09:19:39 +0000

    added alias inlinedocs to function package.skeleton.dx (to be displayed on ?inlinedocs) added an example to package.skeleton.dx added the silly package to inst directory, so that a package for examples is available at installation time
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inlinedocs/inst/silly/R/silly.R
@@ -0,0 +1,48 @@
+silly.example <- function
+### this function does nothing in particular and does it very well
+(
+ ##title<<Simple function arguments
+ first,         ##<< the first argument with a multi-line description
+ ## which I really have to put here rather than explaining in the details.
+ second=        ##<< the second argument with a list default value
+ ## and descriptions of each of the elements
+ ##describe<<
+ list(this="that", ##<< whichness
+      the="other", ##<< of the
+      rhubarb="stew", ##<< why
+      foo="bar"),
+ ##end<<
+ third ##<< an argument that does nothing
+ )
+{
+  ##description<<why should I add to description?
+  ##details<<
+  ## if second is TRUE then first is returned
+  if ( second ){
+    ##alias<<Long silly alias
+    res <- first
+  } else {
+    ##details<<
+    ## if second is not TRUE then a list is returned
+    ##describe<<The contents of the list are:
+    res <- list(x=7, ##<< x coordinate
+                z= ##<< z describes everything else
+                ##describe<<
+                list(colour=green, ##<< colour of line
+                     width=2),     ##<< width of line
+                ##end<<
+                ## and this line should get into documentation for z
+                y=10)##<< y coordinate
+  }
+  ##note<< a note
+  ##references<< a reference
+  ##seealso<< \code{\link{test}}
+  ##keyword<<documentation utilities
+  invisible(res)
+  ### invisible something not unrelated to first
+}
+
+inlinedocExample(silly.example) <- function(){
+	#These lines got to the examples section in the documentation.
+	res <- silly.example(first="first",second=TRUE, third=3)
+}
