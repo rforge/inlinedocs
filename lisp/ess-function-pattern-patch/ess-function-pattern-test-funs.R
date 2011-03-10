d4d213e11a7aaed818aac54752da2662826e88f5
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-10 14:05:48 +0000

    lisp code for matching weird inlinedocs functions in ess C-c C-f
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@216 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ lisp/ess-function-pattern-patch/ess-function-pattern-test-funs.R
@@ -0,0 +1,29 @@
+testfun <- function(foo){
+  bar
+}
+testfun <- function
+(foo){
+  bar
+}
+oneline <- function(foo)bar
+comments <- function
+### some docs
+### more
+(arg,
+ arg2
+ ){
+  foo
+###
+}
+
+comments.and.ex <- structure(function
+### some docs
+### more
+(arg,
+ arg2
+ ){
+  foo
+###
+},ex=function(){
+  some.code
+})
