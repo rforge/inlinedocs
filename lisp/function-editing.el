e85cf9f93a09ac098991a098c57c71f5b522769f
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-11 15:55:07 +0000

    ideas for formatting funs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@219 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ lisp/function-editing.el
@@ -0,0 +1,11 @@
+(defun ess-inlinedocs-add-example ()
+  "add example template using structure() and leave the cursor in
+  the new function body"
+  (interactive)
+)
+
+(defun ess-inlinedocs-format-args ()
+  "break function arguments apart and add blank inlinedocs comments"
+  (interactive)
+)
+
