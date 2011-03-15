947cc2433e7d4aa91349712e53c169483db679ef
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-15 13:54:06 +0000

    move cursor
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@227 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- lisp/function-editing.el
+++ lisp/function-editing.el
@@ -1,18 +1,25 @@
+(defvar ess-inlinedocs-example-start "},ex=function(){\n  "
+  "text to insert for inlinedocs examples")
+
 (defun ess-inlinedocs-add-example ()
   "add example template using structure() and leave the cursor in
   the new example function body"
   (interactive)
-  (save-excursion
-    (let* ((beg-end (ess-end-of-function))
-	   (beg (nth 0 beg-end))
-	   (end (nth 1 beg-end))
-	   name)
-      (goto-char beg)
-      (re-search-forward "function(" nil t)
-      (replace-match "structure(function(")
-      (goto-char end)
-      (re-search-forward "}" nil t)
-      (replace-match "},ex=function(){\n  \n})"))))
+  (let ((REP (concat ess-inlinedocs-example-start "\n})")))
+    (save-excursion
+      (let* ((beg-end (ess-end-of-function))
+	     (beg (nth 0 beg-end))
+	     (end (nth 1 beg-end))
+	     name)
+	(goto-char beg)
+	(re-search-forward "\\(function(\\)" nil t)
+	(replace-match "structure(\\1")
+	(goto-char end)
+	(re-search-forward "}" nil t)
+	(replace-match REP)))
+    (re-search-forward ess-inlinedocs-example-start)
+))
+  
 
 (defun ess-inlinedocs-format-args ()
   "break function arguments apart and add blank inlinedocs comments"
