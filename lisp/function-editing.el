6ccedc319d9064dbbe0cfb4a3dfd5459480eaab4
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-15 13:23:02 +0000

    first try
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- lisp/function-editing.el
+++ lisp/function-editing.el
@@ -1,8 +1,18 @@
 (defun ess-inlinedocs-add-example ()
   "add example template using structure() and leave the cursor in
-  the new function body"
+  the new example function body"
   (interactive)
-)
+  (save-excursion
+    (let* ((beg-end (ess-end-of-function))
+	   (beg (nth 0 beg-end))
+	   (end (nth 1 beg-end))
+	   name)
+      (goto-char beg)
+      (re-search-forward "function(" nil t)
+      (replace-match "structure(function(")
+      (goto-char end)
+      (re-search-forward "}" nil t)
+      (replace-match "},ex=function(){\n  \n})"))))
 
 (defun ess-inlinedocs-format-args ()
   "break function arguments apart and add blank inlinedocs comments"
