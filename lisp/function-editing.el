16d465700e8ddc85c9e7e33ec9508787c437421f
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-15 14:55:39 +0000

    arg fun works for last in buffer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@229 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- lisp/function-editing.el
+++ lisp/function-editing.el
@@ -12,16 +12,35 @@
 	   (REP (concat ess-inlinedocs-example-start "\n})"))
 	   name)
       (goto-char beg)
-      (re-search-forward "\\(function(\\)" nil t)
+      (re-search-forward "\\(function\\)" nil t)
       (replace-match "structure(\\1")
       (goto-char end)
       (re-search-forward "}" nil t)
       (replace-match REP)))
   (re-search-forward ess-inlinedocs-example-start))
-  
+ 
+(defvar ess-inlinedocs-comment-prefix "\n### \n"
+  "what to insert after a argument or description")
 
+(defvar ess-inlinedocs-arg-regexp "\\([^,)\n]+\\)"
+  "regexp used to match argument names")
+;;(setq ess-inlinedocs-arg-regexp "\\([^,)\n]+\\)")
 (defun ess-inlinedocs-format-args ()
   "break function arguments apart and add blank inlinedocs comments"
   (interactive)
+  (ess-mark-function)
+  (re-search-forward "\\(function\\)" nil t)
+  (replace-match (concat "\\1" ess-inlinedocs-comment-prefix)) ;description
+  (while
+      (re-search-forward (concat "\\([ (,]\\)" ess-inlinedocs-arg-regexp "\\(,\\)") nil t)
+    (replace-match (concat "\\1\\2\\3" ess-inlinedocs-comment-prefix " "))
+    (beginning-of-line))
+  (re-search-forward (concat ess-inlinedocs-arg-regexp "\\(){\\)"))
+  (replace-match (concat "\\1" ess-inlinedocs-comment-prefix " \\2"))
+  (let ((lookfor "function\n### "))
+    (re-search-backward lookfor)
+    (re-search-forward lookfor))
 )
+
+
 
