5208ffb267ec96809012051a37031ed8ce0682d9
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-07 13:00:52 +0000

    examples are correctly parsed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@244 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tex/jss/check.R
+++ tex/jss/check.R
@@ -1,3 +1,13 @@
 Stangle("inlinedocs.Rnw")
-library(inlinedocs)
-make.package.and.check("inlinedocs.R")
+e <- new.env()
+sys.source("inlinedocs.R",e,keep.source=TRUE)
+docs <- extract.docs.file("inlinedocs.R")
+for(N in names(e$.result)){
+  expected <- e$.result[[N]]
+  generated <- docs[[N]][names(expected)]
+  m <- rbind(unlist(expected),unlist(generated))
+  same <- apply(m,2,function(x)x[1]==x[2])
+  if(!all(same)){
+    print(m[,!same])
+  }
+}
