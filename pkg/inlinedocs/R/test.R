a7875ff067a4dc878f942867c5a00c81dbf9f327
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-27 11:55:21 +0000

    disable recursive R CMD check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@275 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inlinedocs/R/test.R
+++ pkg/inlinedocs/R/test.R
@@ -46,8 +46,13 @@ test.file <- function
     stop("extracted some unexpected documentation objects!")
   }
   ## finally make a package using this file and see if it passes
-  ## without warnings
-  if(!is.null(e$.dontcheck))return()
+  ## without warnings TDH 27 May 2011 added !interactive() since
+  ## recursive calling R CMD check seems to systematically
+  ## fail... ERROR: startup.Rs not found. This file is usually copied
+  ## to the check directory and read as a .Rprofile, as done in
+  ## tools:::.runPackageTests ... is this a bug in R? Anyway for now
+  ## let's just not run the R CMD check.
+  if(!is.null(e$.dontcheck) || !interactive())return()
   make.package.and.check(f,parsers,verbose)
   if(verbose)cat("\n")
 }
@@ -62,14 +67,11 @@ make.package.and.check <- function
  verbose=TRUE
 ### print the check command line?
  ){
-  print(f)
   pkgname <- sub("[.][rR]$","",basename(f))
-  print(pkgname)
   pkgdir <- file.path(tempdir(),pkgname)
-  print(pkgdir)
   if(file.exists(pkgdir))unlink(pkgdir,recursive=TRUE)
   rdir <- file.path(pkgdir,"R")
-  print(rdir)
+  if(verbose)print(rdir)
   dir.create(rdir,recursive=TRUE)
   desc <- system.file(file.path("silly","DESCRIPTION"),package="inlinedocs")
   file.copy(desc,pkgdir)
@@ -78,10 +80,10 @@ make.package.and.check <- function
   cmd <- sprintf("%s CMD check %s",file.path(R.home("bin"), "R"),pkgdir)
   if(verbose)cat(cmd,"\n")
   checkLines <- system(cmd,intern=TRUE)
-  warnLines <- grep("WARNING",checkLines,value=TRUE)
+  warnLines <- grep("(WARNING|ERROR)",checkLines,value=TRUE)
   if(length(warnLines)>0){
     print(warnLines)
-    stop("WARNING encountered in package check!")
+    stop("ERROR/WARNING encountered in package check!")
   }
 }
 
