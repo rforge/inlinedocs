df552b412beb8cc7cb2305b29fbe34e5b5e05c80
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-07 08:31:21 +0000

    dont annotate tangled R code to avoid extracting superfluous docs
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@289 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tex/jss/check.R
+++ tex/jss/check.R
@@ -1,13 +1,14 @@
-Stangle("inlinedocs.Rnw")
+Stangle("inlinedocs.Rnw",annotate=FALSE)
 e <- new.env()
 sys.source("inlinedocs.R",e,keep.source=TRUE)
 docs <- extract.docs.file("inlinedocs.R")
 for(N in names(e$.result)){
   expected <- e$.result[[N]]
   generated <- docs[[N]][names(expected)]
-  m <- rbind(unlist(expected),unlist(generated))
-  same <- apply(m,2,function(x)x[1]==x[2])
+  m <- cbind(expected=unlist(expected),generated=unlist(generated))
+  same <- apply(m,1,function(x)x[1]==x[2])
   if(!all(same)){
-    print(m[,!same])
+    print(N)
+    print(m[!same,])
   }
 }
