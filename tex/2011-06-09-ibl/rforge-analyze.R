67898bc0e589f88a95c22bdbeffc5d79e815180a
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-05-15 07:23:15 +0000

    make png
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/inlinedocs@361 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tex/2011-06-09-ibl/rforge-analyze.R
+++ tex/2011-06-09-ibl/rforge-analyze.R
@@ -1,15 +1,18 @@
 sorted.projects <- read.csv("project.stats.csv",header=TRUE,
                             colClasses=c("POSIXct","factor","integer"))
 sorted.projects$count <- 1:nrow(sorted.projects)
+filename <- format(Sys.time(),"%Y-%m-%d-R-Forge-project-registration-over-time.png")
+png(filename,width=800)
 par(mar=c(5,4,4,4))
 plot(count~registered,sorted.projects,type="s",las=1,
-     main="Number of R-Forge projects is linearly increasing",
      xlab="Date of project registration",
      ylab="Total number of projects on R-Forge")
+title("Number of R-Forge projects is linearly increasing",line=3)
 last.line <- tail(sorted.projects,1)
 last.date <- last.line$registered
 axis(3,last.date,format(last.date,"%e %B %Y"))
 axis(4,last.line$count,las=1)
+dev.off()
 
 users <- read.csv("project.users.csv",header=TRUE,
                   colClasses=c("factor","factor"))
